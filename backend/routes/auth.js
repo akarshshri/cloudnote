4eb81632acc4b9a16cab69c2b42850d8db3fcc45
akarshshri <akarshshrivastava97@gmail.com>
2022-01-11 01:35:54 +0530

    Updated code to fix buy with DB connect

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,11 +5,11 @@ const app = express()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
-router.post('/',[
+router.post('/createuser',[
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
     body('name').isLength({ min: 3 }),
-],(req, res)=>{
+],async (req, res)=>{
     console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -20,13 +20,24 @@ router.post('/',[
             email: req.body.email,
             password: req.body.password,
         }).then(user => res.json(user))
-        .catch(err=>{
-            res.json({
-                error: 'This email already exists'
-            })
+         .catch(err=>{
+           /*  res.json({
+                error: 'This email already exists' */
+
+                user =  User.findOne({
+                    email: req.body.email
+                });
+                
+                if(user){
+                    return res.status(400).json("Email Exists");
+                }
+                
+          /*   })  */
         })
     
     //res.send(req.body)
-})
 
+
+
+})
 module.exports = router
